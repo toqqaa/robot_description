351ef10bdfac3dcdea131acce5e42f1fa4674821
Toka Essam <145803764+toqqaa@users.noreply.github.com>
2024-12-27 15:04:28 +0200

    Update robot_launch.py

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -16,29 +16,42 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
 
     # Setup project paths
-    pkg_project_description = get_package_share_directory('robot_description')
+    pkg_project_description = get_package_share_directory('yahboomcar_description')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
 
 
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'robot_model', 'robot.sdf')
+    sdf_file  =  os.path.join(pkg_project_description, 'models', 'ackermann_r2_car', 'yahboomcar_R2.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
     # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'robot_launch.py')),
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_description,
             'worlds',
-            'world.sdf'
+            'default.sdf'
         ])}.items(),
     )
-
+    # Static TF base_link -> mono_camera
+    # .15 0 .25 0 0 1.5707
+    cam_x = 0.1284 # 0.05
+    cam_y = 0.0
+    cam_z = 0.07444 #0.15 
+    cam_roll = radians(-90.0)
+    cam_pitch = 0.0
+    cam_yaw = radians(-90.0)
+    cam_tf_node = Node(
+        package='tf2_ros',
+        name='base2depth_tf_node',
+        executable='static_transform_publisher',
+        # arguments=[str(cam_x), str(cam_y), str(cam_z), str(cam_yaw), str(cam_pitch), str(cam_roll), 'base_link', 'camera_link'],
+        arguments=[str(cam_x), str(cam_y), str(cam_z), str(cam_yaw), str(cam_pitch), str(cam_roll), 'camera_link', 'optical_camera_link'],
         
-    
+    )
     # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
     robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -61,26 +74,18 @@ def generate_launch_description():
         package='ros_gz_bridge',
         executable='parameter_bridge',
         parameters=[{
-            'config_file': os.path.join(pkg_project_description, 'config', 'gz_bridge.yaml'),
+            'config_file': os.path.join(pkg_project_description, 'config', 'ros_gz_bridge.yaml'),
             'qos_overrides./tf_static.publisher.durability': 'transient_local',
         }],
         output='screen'
     )
-
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
-        output='screen'
-    )
-
     
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         output='screen',
         name='sim_rviz2',
-        arguments=['-d' + os.path.join(pkg_project_description, 'rviz', 'rahal.rviz')]
+        arguments=['-d' + os.path.join(pkg_project_description, 'rviz', 'yahboomcar.rviz')]
     )
     return LaunchDescription([
         gz_sim,
