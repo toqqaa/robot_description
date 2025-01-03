2c73a7da0bde6f176ae5cda1beab46c277de0c6f
Toka Essam <145803764+toqqaa@users.noreply.github.com>
2025-01-03 04:14:11 +0200

    Update robot_launch.py

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_description,
             'worlds',
-            'world.sdf'
+            'turtlebot3_world.sdf'
         ])}.items(),
     )
 
